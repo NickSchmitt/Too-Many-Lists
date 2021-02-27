6d3acb16c9ba0928971518780781d76b0fa7a3c4
nickschmitt <nickschmitt@gmail.com>
2021-02-26 18:01:38 -0800

    Refactor list from enum to struct
    List struct with a link enum, and a link enum with a Node struct. This helps us 1) Remove extra junk from list tail, 2) Uses null pointer optimization, 3) Uniformly allocates all elements

--- src/first.rs
+++ src/first.rs
@@ -1,6 +1,13 @@
-pub enum List {
-    Empty,
-    ElemThenEmpty(i32),
-    ElemThenNotEmpty(i32, Box<List>)
+pub struct List {
+    head: Link,
 }
 
+enum Link {
+    Empty,
+    More(Box<Node>),
+}
+
+struct Node {
+    elem: i32,
+    next: Link,
+}
