ba6ef8951f5129d2cf870313fd225feee651f234
nickschmitt <nickschmitt@gmail.com>
2021-02-27 14:01:29 -0800

    List instantiation with impl List { pub fn new(){} }

--- src/first.rs
+++ src/first.rs
@@ -2,6 +2,14 @@ pub struct List {
     head: Link,
 }
 
+impl List {
+    pub fn new() -> Self {
+        List {
+            head: Link::Empty
+        }
+    }
+}
+
 enum Link {
     Empty,
     More(Box<Node>),
